51e50f3d358132d200925029d33c5641dd0a4b8d
Arsyad2009 <105086466+Arsyad2009@users.noreply.github.com>
2025-01-16 16:36:41 +0700

    update 1.2

--- db_lkskabtegal.sql
+++ db_lkskabtegal.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 16 Jan 2025 pada 00.21
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.1.25
+-- Generation Time: Jan 16, 2025 at 10:33 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,44 +24,93 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `kontenlomba`
+--
+
+CREATE TABLE `kontenlomba` (
+  `id_kontenlomba` int(11) NOT NULL,
+  `lomba.nama_lomba` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lomba`
+--
+
+CREATE TABLE `lomba` (
+  `id_lomba` int(11) NOT NULL,
+  `nama_lomba` varchar(50) NOT NULL,
+  `keteranganlomba` varchar(255) NOT NULL,
+  `created_at` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `lomba`
+--
+
+INSERT INTO `lomba` (`id_lomba`, `nama_lomba`, `keteranganlomba`, `created_at`) VALUES
+(9, 'ITNSA', 'rue.iux', '2025-01-16 ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `id_user` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `validasi` enum('Sudah','Belum') NOT NULL,
-  `level` enum('Admin','koordinator','pendamping/peserta') NOT NULL,
+  `validasi` enum('Belum','Sudah') NOT NULL,
+  `level` enum('Admin','koordinator','pendamping','peserta') NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id_user`, `username`, `password`, `validasi`, `level`, `created_at`) VALUES
-(1, 'admin', 'admin', 'Sudah', 'Admin', '2025-01-15 15:01:20');
+(1, 'admin', 'admin', 'Sudah', 'Admin', '2025-01-15 15:01:20'),
+(2, 'User1', 'user1', 'Belum', 'koordinator', '2025-01-16 04:09:55'),
+(3, 'user', 'user123', 'Sudah', 'koordinator', '2025-01-16 04:00:34'),
+(4, 'pendamping1', 'pendamping1', 'Sudah', 'pendamping', '2025-01-16 04:09:34'),
+(5, 'peserta1', 'peserta1', 'Sudah', 'peserta', '2025-01-16 04:09:34'),
+(6, 'pendamping2', 'pendamping2', 'Belum', 'pendamping', '2025-01-16 07:12:40'),
+(7, 'peserta2', 'peserta2', 'Belum', 'peserta', '2025-01-16 07:26:27');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `lomba`
+--
+ALTER TABLE `lomba`
+  ADD PRIMARY KEY (`id_lomba`);
+
+--
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `lomba`
+--
+ALTER TABLE `lomba`
+  MODIFY `id_lomba` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
